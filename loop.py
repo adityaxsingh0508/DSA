3ca9d1119b706ad756f8e7ae0b0baac288718b8b
adityaxsingh0508 <adityasingh052003@gmail.com>
2025-05-19 23:59:26 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,29 @@
+
+# For loop example
+print("For loop:")
+for i in range(1, 6):
+    print(i)
+
+# While loop example
+print("\nWhile loop:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+
+# For loop with break
+print("\nFor loop with break:")
+for i in range(1, 10):
+    if i == 6:
+        break
+    print(i)
+
+# For loop with continue
+print("\nFor loop with continue:")
+for i in range(1, 6):
+    if i == 3:
+        continue
+    print(i)
+
+
+
