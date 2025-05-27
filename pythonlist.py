24b66f9057db95f70a0d38fcb6135e3298f1cfca
adityaxsingh0508 <adityasingh052003@gmail.com>
2025-05-27 23:58:46 +0530

    Create pythonlist.py

---
+++ pythonlist.py
@@ -0,0 +1,18 @@
+# Creating an array (list)
+arr = [10, 20, 30, 40]
+
+# Accessing elements
+print(arr[0])      # Output: 10
+
+# Modifying elements
+arr[1] = 25
+
+# Adding elements
+arr.append(50)
+
+# Removing elements
+arr.remove(30)     # removes the value 30
+
+# Iterating through the array
+for item in arr:
+    print(item)
